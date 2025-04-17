267a122c4e8c26cf76a4f89f7eff96ed20b1522f
jeportie <jeportie@student.42.fr>
2025-04-17 09:46:04 +0000

    push pause

--- ex01/src/class/Brain.hpp
+++ ex01/src/class/Brain.hpp
@@ -21,9 +21,10 @@ public:
 	Brain(const Brain& src);
 	~Brain(void);
 
+	Brain&		operator=(const Brain& rhs);
+
 	void		pushIdea(std::string const idea);
 	std::string	getIdea(int i);
-	Brain&		operator=(const Brain& rhs);
 
 private:
 	std::string _ideas[100];
